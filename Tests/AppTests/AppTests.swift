c8f97010bd5a51e5e06d07bee5b2815e0b66db6e
edhoguntur <edho.guntur@gmail.com>
2023-10-28 14:20:07 +0700

    Add Test case Index poke controller and sync

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -12,4 +12,26 @@ final class AppTests: XCTestCase {
             XCTAssertEqual(res.body.string, "Hello, world!")
         })
     }
+    
+    func testIndex() async throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try await configure(app)
+
+        try app.test(.GET, "pokemons", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "It's Pokedex")
+        })
+    }
+    
+    func testSync() async throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try await configure(app)
+
+        try app.test(.GET, "pokemons/sync", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "Pokedex is synced successfully!")
+        })
+    }
 }
